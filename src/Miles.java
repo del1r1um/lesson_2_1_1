5a2174aa55c29fbb3455d08f6d1d8a95cabb37c8
Aleksandr Samsonov <therion@luvu.ru>
2021-07-16 09:14:42 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        int ticketCost = 1000;
+        int milleBonus = ticketCost / 20;
+        System.out.println("Количество начисленных миль: " + milleBonus);
+    }
+}
